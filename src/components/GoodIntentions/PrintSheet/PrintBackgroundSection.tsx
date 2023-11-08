3ec41d039c3e5f364264203e7aa4221c37a31f3c
stevenbennett-yvr <stevenbennett90@gmail.com>
2023-11-07 17:33:05 -0800

    ui for Backgrounds Printout

--- src/components/GoodIntentions/PrintSheet/PrintBackgroundSection.tsx
+++ src/components/GoodIntentions/PrintSheet/PrintBackgroundSection.tsx
@@ -1,7 +1,8 @@
-import { Card, Table, Text, Stack, List, Divider, Center, SimpleGrid } from "@mantine/core"
+import { Card, Table, Text, Stack, List, Divider, Center, SimpleGrid, Group } from "@mantine/core"
 
 import { Kindred } from "../../../data/GoodIntentions/types/Kindred"
 import { kindredBackgrounds, V5BackgroundRef, backgroundData, v5BackgroundLevel, v5AdvantageLevel } from "../../../data/GoodIntentions/types/V5Backgrounds"
+import { Spheres } from "../../../data/GoodIntentions/types/V5Spheres";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleDown, faCircleUp } from "@fortawesome/free-solid-svg-icons";
@@ -25,33 +26,49 @@ const BackgroundSection = ({ kindred }: PrintSheetProps) => {
         const backgroundInfo = backgroundData.find((b) => b.name === background.name)
         if (!backgroundInfo) { return null }
         return (
-            <Center>
-                <Card w={200}>
-                    <Table>
-                        <thead>
-                            <tr>
-                                <td>
+            <Card w={200}>
+                <Table>
+                    <thead>
+                        <tr>
+                            <td>
+                                <Group>
+                                    <FontAwesomeIcon icon={backgroundInfo.icon} style={{ color: "#e03131" }} />
                                     {backgroundInfo.name} {v5BackgroundLevel(background).level}
-                                </td>
+                                </Group>
+                            </td>
+                        </tr>
+                        {background.sphere && background.sphere.length > 0 ?
+                            <tr>
+                                <Stack justify="flex-start" align="flex-start" spacing={3}>
+                                    <Text size="sm">Spheres:</Text>
+                                    <Stack spacing={3}>
+                                        {background.sphere.map((s) => (
+                                            <Group>
+                                            <Text size="xs">{s} </Text>
+                                            <FontAwesomeIcon icon={Spheres[s].symbol} style={{ color: "#e03131" }} />
+                                            </Group>
+                                        ))}
+                                    </Stack>
+                                </Stack>
                             </tr>
-                        </thead>
-                        <tbody>
-                            {background.advantages.map((advantage) => {
-                                if (!backgroundInfo.advantages || v5AdvantageLevel(advantage).level < 1) { return null }
-                                const advantageInfo = backgroundInfo.advantages.find((a) => a.name === advantage.name)
-                                const icon = advantageInfo?.type === "disadvantage" ? flawIcon() : meritIcon()
-                                return (
-                                    <tr key={advantage.name}>
-                                        <td>
-                                            <Text>{icon} &nbsp;{advantage.name} {v5AdvantageLevel(advantage).level}</Text>
-                                        </td>
-                                    </tr>
-                                )
-                            })}
-                        </tbody>
-                    </Table>
-                </Card>
-            </Center>
+                            : <></>}
+                    </thead>
+                    <tbody>
+                        {background.advantages.map((advantage) => {
+                            if (!backgroundInfo.advantages || v5AdvantageLevel(advantage).level < 1) { return null }
+                            const advantageInfo = backgroundInfo.advantages.find((a) => a.name === advantage.name)
+                            const icon = advantageInfo?.type === "disadvantage" ? flawIcon() : meritIcon()
+                            return (
+                                <tr key={advantage.name}>
+                                    <td>
+                                        <Text>{icon} &nbsp;{advantage.name} {v5AdvantageLevel(advantage).level}</Text>
+                                    </td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                </Table>
+            </Card>
         )
     }
 
@@ -85,8 +102,8 @@ const BackgroundSection = ({ kindred }: PrintSheetProps) => {
             <Stack>
                 <Divider my="sm" label="Backgrounds" labelPosition="center" />
                 <SimpleGrid cols={globals.isPhoneScreen ? 1 : 3}>
-                        { loresheetCard() }
-                        { sortedBackgrounds.map((b) => backgroundCard(b) )}
+                    {loresheetCard()}
+                    {sortedBackgrounds.map((b) => backgroundCard(b))}
                 </SimpleGrid>
             </Stack>
         </Center>
